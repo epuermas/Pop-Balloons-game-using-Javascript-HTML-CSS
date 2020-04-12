5140629aad7222ec8511600c731e9c2bf7cfadaf
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-04-12 13:27:57 +0000

    javascript added

---
+++ app.js
@@ -0,0 +1,26 @@
+  
+// we declare a new global variable containing an array that represents the ballons map
+let ballonsMap = ['green'];
+let activeBalloons = 20;
+
+// poping a balloon is basically turning his color to null (no color)
+const popBalloon = (position) => {
+    // set the color to null on the balloon position
+    render();
+}
+
+const render = () => {
+    
+    // convert ballons map of colors into real html balloons
+    const ballons = ballonsMap.map((color, position) => {
+        return `<div class="balloon active"></div>`; // <--- render each balloon
+    });
+
+    document.querySelector("#balloon-count").innerHTML = activeBalloons; // <-- rende the balloon count into the DOM
+    document.querySelector("#balloon-map").innerHTML = ballons.join(''); // <-- render the balloons into the DOM
+
+    if(activeBalloons == 0) window.location.reload(); // <--- reload website when no more balloons are left
+}
+
+// this makes the "render" function trigger when the website starts existing
+window.onload = render();
