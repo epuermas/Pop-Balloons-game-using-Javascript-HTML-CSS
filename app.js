04d2fbf98058b784171bedb93154860b0c30a311
Eduardo Puermas <eduardopuermas@hotmail.com>
2020-04-23 14:15:08 +0000

    colors added

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const container = document.querySelector('.container');
 let content = "";
 let balloons = [
-    'pink','black','red','purple'
+    'lightsalmon','salmon','red','firebrick',
+    'yellow','lemonchiffon','khaki','papayawhip',
+    'lime','limegreen','palegreen','olivedrab',
+    'lightcyan','aqua','turquoise','teal',
+    'lightskyblue','steelblue','royalblue','blue',
 ];
 
 balloons.forEach(function(color, i){
-    content = content + '<div class="balloon"></div>';
-    console.log("Loop"+i,content);
-
+    console.log("Loop"+i+" "+color,content);
+    content = content + `<div 
+    class="balloon" 
+    style="background: ${color}"
+    >
+    </div>`;
 });
 
 
